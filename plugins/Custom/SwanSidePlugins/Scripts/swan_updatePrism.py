d4e3be7d9f6dc0c295cc6112c1bae71b23f773e9
Vincannes <vincent.trolard@gmail.com>
2024-07-17 17:21:39 +0200

    grosse refacto

--- plugins/Custom/SwanSidePlugins/Scripts/swan_updatePrism.py
+++ plugins/Custom/SwanSidePlugins/Scripts/swan_updatePrism.py
@@ -6,20 +6,31 @@ import shutil
 import logging
 import datetime
 
+THIS_DIR = os.path.dirname(os.path.dirname(__file__))
+sys.path.append(os.path.join(THIS_DIR, "ExternalModules", "releaser_modules"))
+
+import requests
+from github_release_downloader import AuthSession, get_latest_version, GitHubRepo, get_available_versions
+
 FOLDER_SCRIPTS = os.path.join("C:\\ProgramData", "Prism2")
 ARCHIVE_DIR = os.path.join(FOLDER_SCRIPTS, ".archive")
-# SOURCE_DIR = os.path.join(FOLDER_SCRIPTS, "plugins")
-# DESTIN_DIR = os.path.join(FOLDER_SCRIPTS, "test")
-SOURCE_DIR = os.path.join("Z:\Vincent", "Scripts_SWANSIDE")
-DESTIN_DIR = os.path.join(FOLDER_SCRIPTS, "plugins")
+# DESTIN_DIR = os.path.join(FOLDER_SCRIPTS, "plugins")
+DESTIN_DIR = os.path.join(FOLDER_SCRIPTS, "test_plugins")
 DEST_VERSION_JSON_FILE = os.path.join(DESTIN_DIR, "version.json")
-SOURC_VERSION_JSON_FILE = os.path.join(SOURCE_DIR, "version.json")
+
 
 try:
     if not os.path.exists(ARCHIVE_DIR):
         os.mkdir(ARCHIVE_DIR)
 except:
     logging.info("BUG Path {} does't exist".format(ARCHIVE_DIR))
+
+REPO = GitHubRepo("Vincannes", "SwanSidePipeline")
+AuthSession.init(REPO)
+
+
+def version_tuple(version):
+    return tuple(map(int, version.split('.')))
 
 
 def get_archive_versions():
@@ -37,11 +48,11 @@ def get_archive_versions():
 def get_version(version_json):
     f = open(version_json)
     data = json.load(f)
-    return float(data.get("version", 1.0))
+    return data.get("version", 1.0)
 
 
 def zip_directory(directory):
-    shutil.make_archive(directory, 'zip', SOURCE_DIR)
+    shutil.make_archive(directory, 'zip', DESTIN_DIR)
     return "{}.zip".format(directory)
 
 
@@ -68,15 +79,6 @@ def create_save_file(version):
     return formated_path
 
 
-def copy_files(directory):
-    for root, dirs, files in os.walk(directory):
-        for f in files:
-            shutil.copy2(
-                os.path.join(root, f),
-                os.path.join(directory, f)
-            )
-
-
 def clean_files(directory):
     for filename in os.listdir(directory):
         file_path = os.path.join(directory, filename)
@@ -89,19 +91,44 @@ def clean_files(directory):
             print('Failed to delete %s. Reason: %s' % (file_path, e))
 
 
-def copy_files():
-    for root, dirs, files in os.walk(SOURCE_DIR):
+def get_release_versions():
+    return sorted([f"{i}" for i in get_available_versions(REPO)])
+
+
+def get_release_url(release_version):
+    request_url = f"https://api.github.com/repos/{REPO.user}/{REPO.repo}/releases/tags/{release_version}"
+    data = json.loads(requests.get(request_url, headers=AuthSession.header).text)
+    if 'message' in data:
+        return
+    name = data.get("name")
+    zip_url = data.get("zipball_url")
+    return name, zip_url
+
+
+def download_release(name, zip_url):
+    response = requests.get(
+        zip_url, stream=True, headers=AuthSession.header
+    )
+    zip_file = os.path.join(FOLDER_SCRIPTS, "{}.zip".format(name))
+    with open(zip_file, 'wb') as file:
+        for i, data in enumerate(response.iter_content(2 ** 20, )):
+            file.write(data)
+    return zip_file
+
+
+def copy_files(dir_path):
+    for root, dirs, files in os.walk(dir_path):
         for f in files:
-            full_file_name = os.path.join(root, f)
-            if os.path.isfile(full_file_name):
-                dest = full_file_name.replace(SOURCE_DIR, DESTIN_DIR)
-                if any([a in root for a in [".git"]]):
-                    print("la")
-                    print(root)
+            zip_file_path = os.path.join(root, f)
+            if os.path.isfile(zip_file_path):
+                dest = zip_file_path.replace(dir_path, FOLDER_SCRIPTS)
+                if any([a in root for a in [".git", ".archive"]]):
                     continue
                 if not os.path.exists(os.path.dirname(dest)):
                     os.makedirs(os.path.dirname(dest))
-                shutil.copy(full_file_name, dest)
+                if os.path.exists(dest):
+                    os.remove(dest)
+                shutil.copy(zip_file_path, dest)
 
 
 def rollback():
@@ -113,20 +140,35 @@ def rollback():
 
 
 def has_to_run():
-    src_json_version = get_version(SOURC_VERSION_JSON_FILE)
+    last_release_version = get_release_versions()[-1]
     dst_json_version = get_version(DEST_VERSION_JSON_FILE)
-    return src_json_version < dst_json_version
+    return version_tuple(last_release_version) > version_tuple(dst_json_version)
 
 
 def run():
-    src_json_version = get_version(SOURC_VERSION_JSON_FILE)
-    save_dir_zip = create_save_file(src_json_version)
-    zip_file = zip_directory(save_dir_zip)
-    clean_files(DESTIN_DIR)
-    copy_files()
+    last_release_version = get_release_versions()[-1]
+    dst_json_version = get_version(DEST_VERSION_JSON_FILE)
+
+    save_dir_zip = create_save_file(dst_json_version)
+    zip_directory(save_dir_zip)
+
+    name, zip_realease_url = get_release_url(last_release_version)
+    zip_file = download_release(name, zip_realease_url)
+    # clean_files(DESTIN_DIR)
+    unzip_directory(zip_file, FOLDER_SCRIPTS)
+
+    for i in os.listdir(FOLDER_SCRIPTS):
+        if i.startswith("Vincannes"):
+            github_folder = os.path.join(FOLDER_SCRIPTS, i)
+            copy_files(github_folder)
+            os.remove(github_folder)
+    shutil.copy(zip_file, os.path.join(ARCHIVE_DIR, os.path.basename(zip_file)))
 
 
 if __name__ == "__main__":
     # run()
     from pprint import pprint
-    rollback()
+    # rollback()
+
+    out_dir = "D:\\Desk\\projets\\test.zip"
+    run()
